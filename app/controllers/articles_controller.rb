d501cfd9fa6be34a2e26ad3fbc202669dbf55069
simonjamain <jamain.simon@gmail.com>
2017-09-19 09:12:19 +0000

    feature-articles: création du modèle d'article, affichage d'un article simplifié (titre et description)

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,33 @@
+class ArticlesController < ApplicationController
+  def index
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    
+    @article.save
+    redirect_to @article
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+  
+end
