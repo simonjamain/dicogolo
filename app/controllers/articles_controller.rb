857ab24d3c4ab8abb5d47e85cf3fed4981ea1a43
simonjamain <jamain.simon@gmail.com>
2017-09-19 09:50:45 +0000

    feature-articles: création d'un nouvel article possible

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,12 +8,13 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
     @article = Article.new(article_params)
-    
     @article.save
+    
     redirect_to @article
   end
 
