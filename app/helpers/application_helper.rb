7d31037d8862331efd68314369773e43b37e988f
simonjamain <jamain.simon@gmail.com>
2017-09-26 18:13:00 +0000

    feature-articles: updated all fabs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  
+  def mainFab(fabUrl, fabFontawesomeIconName)
+     render partial: "/common/fab", locals: {fabUrl: fabUrl, fabFontawesomeIconName: fabFontawesomeIconName}
+  end
+  def secondaryFab(fabUrl, fabFontawesomeIconName)
+     render partial: "/common/secondary_fab", locals: {fabUrl: fabUrl, fabFontawesomeIconName: fabFontawesomeIconName}
+  end
 end
