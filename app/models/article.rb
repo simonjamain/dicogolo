c6d4f336dd79820fd653440f4022ebff7fc3d308
simonjamain <jamain.simon@gmail.com>
2017-09-26 17:34:55 +0000

    feature-articles: placeholder image added

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ActiveRecord::Base
     has_attached_file :illustration,
                       :styles => { :maxi => "700x466#",
-                                   :mini => "600x400#"}
+                                   :mini => "600x400#"},
+                      :default_url => "image_placeholder.svg"
     validates_attachment_content_type :illustration, :content_type => /\Aimage\/.*\Z/
 end
