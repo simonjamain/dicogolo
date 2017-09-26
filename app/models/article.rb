548f4a1542828fb202eb299722dcf6a44a56c0cc
simonjamain <jamain.simon@gmail.com>
2017-09-26 08:39:51 +0000

    feature-articles: maxi article design ok

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
-    has_attached_file :illustration
+    has_attached_file :illustration,
+                      :styles => { :maxi => "700x466#",
+                                   :mini => "600x400#"}
     validates_attachment_content_type :illustration, :content_type => /\Aimage\/.*\Z/
 end
--- db/migrate/20170919201335_add_attachment_illustration_to_articles.rb
+++ db/migrate/20170919201335_add_attachment_illustration_to_articles.rb
@@ -1,7 +1,11 @@
 class AddAttachmentIllustrationToArticles < ActiveRecord::Migration
   def self.up
     change_table :articles do |t|
-      t.attachment :illustration
+      t.attachment :illustration,
+        styles: { 
+          maxi: "700x466#",
+          mini: "600x400#"
+        }
     end
   end
 
